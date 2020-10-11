6f646507d20f010c534d1f056c094134ddd373ab
Fish <conner.leuk@gmail.com>
2020-10-11 11:24:58 +0200

    auto restart

--- index.js
+++ index.js
@@ -1,79 +1,13 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+var forever = require('forever-monitor');
 
-
-const client = new Discord.Client();
-
-
-
-require('dotenv').config(/*{ path: './.env' }*/);
-
-let config = require("./config.json");
-
-config.token = process.env.TOKEN
-
-client.config = config;
-
-
-fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-        const event = require(`./events/${file}`);
-        //let eventName = file.split(".")[0];
-        client.on(event.conf.event, event.event.bind(null, client));
-    });
+var child = new (forever.Monitor)('bot.js', {
+  max: 3,
+  silent: false,
+  args: []
 });
 
-//client.commands = new Enmap();
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
+child.on('exit', function () {
+  console.log('bot.js has exited after 3 restarts');
+});
 
-client.bypass = false;
-client.master = client.config.master
-
-fs.readdir("./commands/", (direrr, dirs) =>{
-    if (direrr) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    console.log(dirs)
-    
-    dirs.forEach(dir => {
-        const path = "./commands/"+dir+"/";
-        fs.readdir(path, (err, files) => {
-            if (err) return console.error(err);
-            files.forEach(file => {
-                if (!file.endsWith(".js")) return;
-            
-                let props = require(path+file);
-                console.log(`Loading Command: ${props.help.name}`);
-                client.commands.set(props.help.name, props);
-
-                props.conf.aliases.forEach(alias => {
-                    client.aliases.set(alias, props.help.name);
-                });
-            });
-        });
-
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.sendinfo = function (info){
-    client.channels.cache.get('764105545241329725').send(info);
-}
-
-client.login(config.token);
+child.start();
