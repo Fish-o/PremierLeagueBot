a465dc599833b572b0e0458b3784dd54a55b90b1
Fish <conner.leuk@gmail.com>
2020-10-12 18:19:05 +0200

    music + other stuff

--- events/assignTeamRole.js
+++ events/assignTeamRole.js
@@ -1,5 +1,57 @@
 
 const Discord = require('discord.js');
+musconst cooldown = new Map();
+const cooldown_seconds = 3600;
+const ms = require('ms');
+
+
+
+var bar_styles = [
+    '▁▂▃▄▅▆▇█',
+    '⣀⣄⣤⣦⣶⣷⣿',
+    '⣀⣄⣆⣇⣧⣷⣿',
+    '○◔◐◕⬤',
+    '□◱◧▣■',
+    '□◱▨▩■',
+    '□◱▥▦■',
+    '░▒▓█',
+    '░█',
+    '⬜⬛',
+    '▱▰',
+    '▭◼',
+    '▯▮',
+    '◯⬤',
+    '⚪⚫',
+];
+function repeat(s, i) {
+    var r = '';
+    for(var j=0; j<i; j++) r += s;
+    return r;
+}
+function make_bar(p, bar_style, min_size, max_size) {
+    var d, full, m, middle, r, rest, x,
+        min_delta = Number.POSITIVE_INFINITY,
+        full_symbol = bar_style[bar_style.length-1],
+        n = bar_style.length - 1;
+    if(p == 100) return {str: repeat(full_symbol, 10), delta: 0};
+    p = p / 100;
+    for(var i=max_size; i>=min_size; i--) {
+        x = p * i;
+        full = Math.floor(x);
+        rest = x - full;
+        middle = Math.floor(rest * n);
+        if(p != 0 && full == 0 && middle == 0) middle = 1;
+        d = Math.abs(p - (full+middle/n)/i) * 100;
+        if(d < min_delta) {
+            min_delta = d;
+            m = bar_style[middle];
+            if(full == i) m = '';
+            r = repeat(full_symbol, full) + m + repeat(bar_style[0], i-full-1);
+        }
+    }
+    return {str: r, delta: min_delta};
+}
+
 
 
 exports.event = async (client, message) => {
@@ -22,18 +74,29 @@ exports.event = async (client, message) => {
         all_teams.forEach((team_name) => {
             command = `${client.config.prefix}${team_name}`
             if (content.toLowerCase().replace(' ', '').startsWith(`${command}`) || content === command || content.toLowerCase().replace(' ', '') === command) {
-                const team_role = message.guild.roles.cache.find(guild_role => guild_role.name.toLowerCase().replace(' ', '') == team_name.toLowerCase().replace(' ', ''))
+
+                if (cooldown.has(message.author.id) && !message.member.hasPermission('ADMINISTRATOR')) {
+
+                    const timeSince = cooldown.get(message.author.id) - Date.now();
+                    const timeRemaining_ms = cooldown_seconds*1000 - timeSince;
+                    const percentage_remaining = Math.floor(100 - (cooldown_seconds*1000 / timeRemaining_ms) * 100);
+
+                    
+                    return message.channel.send("Please wait "+ms(timeRemaining_ms, { long: true })+', progress: '+make_bar(percentage_remaining, '⣀⣄⣤⣦⣶⣷⣿', 1, 20));
+                }
+
+                const team_role = guild.roles.cache.find(guild_role => guild_role.name.toLowerCase().replace(' ', '') == team_name.toLowerCase().replace(' ', ''))
                 if(!team_role){
                     return message.channel.send(`Could not find team: ${team_name} (${team_name.toLowerCase().replace(' ', '')})`)
                 }
-                const member_roles = message.member.roles.cache;
+                const member_roles = member.roles.cache;
                 const member_team_roles = member_roles.filter(member_role =>all_teams.includes(member_role.name.toLowerCase()))
 
                 if(member_team_roles){
                     console.log(member_team_roles.length)
                     member_team_roles.forEach(async member_team_role => {
                         console.log('Removed team role from member')
-                        message.member.roles.remove(member_team_role, 'Removed a members second team')
+                        member.roles.remove(member_team_role, 'Removed a members second team')
                     })
                 }
 
@@ -43,7 +106,17 @@ exports.event = async (client, message) => {
                     member.roles.remove(home_away_role, 'Removed a members Home Away role, because they left their team a new one')
                 });
 
-                message.member.roles.add(team_role, 'Joined a team the team')
+                member.roles.add(team_role, 'Joined a team the team')
+
+                if(!message.member.hasPermission('ADMINISTRATOR')){
+                    cooldown.set(message.author.id, Date.now());
+                    
+                    setTimeout(() => {
+                        // Removes the user from the set after a minute
+                        cooldown.delete(message.author.id);
+                    }, cooldown_seconds*1000);
+                }
+
                 message.channel.send({
                     embed: {
                       description: `You succesfully joined team: **${team_role.name}**`,
